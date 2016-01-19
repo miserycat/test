29652631562a54fb6753d1914eb7afda27de79b7
shengchao wu <shengchao.wu@lombardrisk.com>
2016-01-19 10:57:01 +0800

    add Demo1

---
+++ Demo1.java
@@ -0,0 +1,15 @@
+package test.guawa;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by shengchao wu on 1/13/2016.
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(new String[]{"aaa","bbb","ccc"});
+        String result = BuildString.buildString(list,",");
+        System.out.print(result);
+    }
+}
